2f79f78c3096b96fc7e66c057588d5959521be0a
Mehedi Abdullah <aamehedi93@gmail.com>
2017-11-24 16:49:06 +0600

    Average of 3 Integer number by using Function.
    This function is Take's something, Return's nothing type. This will produce the Average of three Number from taking from the user.

---
+++ average3integer200.cpp
@@ -0,0 +1,30 @@
+/*
+   The sum of two Integer.
+   1.Start.
+   2.Take three input.
+   3.Make sum of three number.
+   4.Make average of three number.
+   5.Show average in output.
+   5.End.
+ */
+ #include <iostream>
+using namespace std;
+//This function will produce the Average of number's.
+//This function that Takes Something,Returns Nothing.
+
+void average(int, int, int); //Globar Declaration.
+int main()
+{
+        //void average(int, int, int); --> Local Declaration.
+
+        int num1, num2, num3;
+        cout <<"Enter three number to find Average :" <<endl;
+        cin >> num1 >> num2 >> num3;
+        average(num1, num2, num3); //Function call with actual argument && call by value.
+
+}
+void average(int num1, int num2, int num3){ //Function Defination.
+        int average;
+        average = (num1+num2+num3)/ 3;
+        cout << "Average is = " << average;
+}
