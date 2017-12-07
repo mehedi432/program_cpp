56671cf3d52eaf4e61768ee33c9db4ce4ee12030
Mehedi Abdullah <aamehedi93@gmail.com>
2017-12-07 09:37:24 +0600

    Print Natural number
    A simple program to print natural number's..

---
+++ program-233-cpp/printNaturalNumber-3.cpp
@@ -0,0 +1,31 @@
+/*
+   Task-> Print the natural number (1...n)..
+
+    1.Start
+    2.Input the limit of the number.
+    3.Initialize increment.
+    4.While i<=n.
+    5.print(i).
+    6.update(i).
+    7.Stop.
+ */
+ 
+ #include <iostream>
+using namespace std;
+
+int display(int number)
+{
+        for(int i = 1; i <number; i++) {
+                cout << "Natural number's are : " << i <<endl;
+        }
+}
+
+int main()
+{
+        int number;
+        cout << "Enter Limit number :";
+        cin >> number;
+        display(number);
+        //cout <<""
+
+}
