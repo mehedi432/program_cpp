f49b590ea658d8b4d95dc539d98ac33b74cc429c
Mehedi Abdullah <aamehedi93@gmail.com>
2017-12-08 12:04:34 +0600

    Sum of squared numbers
    # A simple program that can add the square value 1^2+2^2+3^2+........ ..+n.(upto n)

---
+++ program-233-cpp/Loop And Decision Control/sqaureNatural number add 5.cpp
@@ -0,0 +1,34 @@
+/*
+   Task-> Add the square value 1^2+2^2+3^2+....up to n'th term.(n==userInput).
+
+   1.Start
+   2.Input howManyterm need for addition.
+   3.Make sum =0, i=1;
+   4.While(i<=n)
+        =>repeat step 5
+   5.If condition is True:
+        sum = sum + i*i;
+        i=i+1;
+   6.cout << Sum || Result.
+   7.End.
+ */
+
+#include <iostream>
+using namespace std;
+float add(int howManyterm){
+        float sum = 0;
+        for(int i = 1; i<= howManyterm; i++) {
+                sum = sum + i*i; //This i*i returns the sqare of that iteation number for the Loop.
+        }
+        return sum;
+}
+
+int main()
+{
+        int howManyterm,display;
+        cout << "Enter how many terms you want to add the square value 1^2+2^2+3^2+........ :"<<endl;
+        cin >> howManyterm;
+        display=add(howManyterm);
+        cout << "The result is :" <<display;
+
+}
