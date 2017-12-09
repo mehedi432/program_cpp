ccc81996e3ee039513838ea4f80c90282a0355b2
Mehedi Abdullah <aamehedi93@gmail.com>
2017-12-09 17:10:04 +0600

    multiplication Of Square Even Number
    A program for multiplication Of Square Even Number using for loop.

---
+++ program-233-cpp/Loop And Decision Control/multiplicationOfSquareEvenNumber-9.cpp
@@ -0,0 +1,34 @@
+/*
+   Task-> Sum of natural series with quabes.
+   1.Start
+   2.Take Input upto term.
+   3.result= 2,i= 2.
+   4.while(i<=n)repeat step 5.
+   5.result = resul*i*i.
+   6.cout << Result.
+ */
+ 
+ #include <iostream>
+using namespace std;
+int multQuabe(int nTerm){
+        int result = 1;
+        for(int i = 2; i<=nTerm; i+=2) {
+                cout << i<<endl;
+                result = result * (i*i);
+
+        }
+        return result;
+
+}
+
+int main()
+{
+        int nTerm,display;
+        cout << "Enter How many term you want to Multiply: "<<endl;
+        cin >> nTerm;
+        display = multQuabe(nTerm);
+        cout << "The result is :" <<display;
+
+
+        return 0;
+}
